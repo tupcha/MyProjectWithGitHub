bde1ce9af072e2082000bfc9f9970e9acb72931c
tupcha <terem.tema2@gmail.com>
2022-12-03 00:56:30 +0300

    New class aDddeed

---
+++ src/main/java/org/example/NewClass.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class NewClass {
+    private String name;
+
+    public NewClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
